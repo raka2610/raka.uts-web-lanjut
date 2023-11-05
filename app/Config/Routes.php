b2434a49786bbbf54380b7d769f2419c7d6d9326
raka2610 <147386834+raka2610@users.noreply.github.com>
2023-11-06 00:50:01 +0700

    Update
    1

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/About', 'About::index');
+$routes->get('/Education', 'Education::index');
